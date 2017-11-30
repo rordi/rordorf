b206fd3aad317cfc77aa3f53fcb4355d949e749a
Dietrich Rordorf <dr@ediqo.com>
2017-11-30 10:17:32 +0100

    styling changes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,10 @@
 import gulp from "gulp";
 import {spawn} from "child_process";
+import fs from "fs";
 import hugoBin from "hugo-bin";
 import gutil from "gulp-util";
 import sass from "gulp-sass";
 import postcss from "gulp-postcss";
-import cssImport from "postcss-import";
-import cssnext from "postcss-cssnext";
 import BrowserSync from "browser-sync";
 import watch from "gulp-watch";
 import webpack from "webpack";
@@ -22,20 +21,24 @@ gulp.task("hugo", (cb) => buildSite(cb));
 gulp.task("hugo-preview", (cb) => buildSite(cb, hugoArgsPreview));
 
 // Build/production tasks
-gulp.task("build", ["sass", "js"], (cb) => buildSite(cb, [], "production"));
-gulp.task("build-preview", ["sass", "js"], (cb) => buildSite(cb, hugoArgsPreview, "production"));
+gulp.task("build", ["truncate", "sass", "js"], (cb) => buildSite(cb, [], "production"));
+gulp.task("build-preview", ["truncate", "sass", "js"], (cb) => buildSite(cb, hugoArgsPreview, "production"));
+
+gulp.task('truncate', function () {
+    fs.truncate("./dist/css/main.css", (err) => {});
+});
 
 gulp.task('sass', function () {
-    // compile sass to css
-    gulp.src("./src/css/*.scss")
+  // compile sass to css
+  gulp.src("./src/css/*.scss")
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest("./dist/css"));
 
-    // post-process compiled CSS with PostCSS
-    gulp.src("./dist/css/main.css")
-        .pipe(postcss([]))
-        .pipe(gulp.dest("./dist/css"))
-        .pipe(browserSync.stream())
+  // post-process compiled CSS with PostCSS
+  gulp.src("./dist/css/main.css")
+      .pipe(postcss([]))
+      .pipe(gulp.dest("./dist/css"))
+      .pipe(browserSync.stream());
 });
 
 // Compile Javascript
@@ -54,14 +57,14 @@ gulp.task("js", (cb) => {
 });
 
 // Development server with browsersync
-gulp.task("server", ["hugo", "sass", "js"], () => {
+gulp.task("server", ["hugo", "truncate", "sass", "js"], () => {
   browserSync.init({
     server: {
       baseDir: "./dist"
     }
   });
   watch("./src/js/**/*.js", () => { gulp.start(["js"]) });
-  watch("./src/css/**/*.scss", () => { gulp.start(["sass"]) });
+  watch("./src/css/**/*.scss", () => { gulp.start(["truncate", "sass"]) });
   watch("./site/**/*", () => { gulp.start(["hugo"]) });
 });
 
