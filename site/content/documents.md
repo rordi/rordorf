ff12c51ca04e0b3ecd3ff7ccc709e3a1b6ac4338
Dietrich Rordorf <dr@ediqo.com>
2018-04-19 09:35:23 +0200

    add documents handling to frontend

---
+++ site/content/documents.md
@@ -0,0 +1,15 @@
+---
+title: "Dokumente"
+date: 2018-04-18T02:00:00+01:00
+draft: false
+---
+<p>
+    Bitte geben Sie das Passwort ein, um die Dokumente anzusehen.
+</p>
+<p>
+    <form id="docform">
+        <input name="docpw" id="docpw" type="text">
+        <button type="submit">Weiter</button>
+    </form>
+</p>
+<div class="documents" id="docwrap"></div>
--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,75 @@
+import axios from "axios";
+
 let Masonry = require('masonry-layout'),
     imagesLoaded = require('imagesloaded'), // required by Masonry for image galleries
     SnowFlakes = require('magic-snowflakes'),
+    AES = require("crypto-js/aes"),
+    SHA256 = require("crypto-js/sha256"),
+    ENCUTF8 = require("crypto-js/enc-utf8"),
     menu = document.getElementById('collapsible'),
     toggler = document.getElementById('toggler'),
     togglericon = document.getElementById('togglericon')
 ;
+
+/**
+ * crypto functions for doc downloads
+ */
+let docform = document.querySelector('#docform');
+if (docform) {
+    let docwrap = document.querySelector('#docwrap');
+    let docpw = document.querySelector('#docpw');
+    let ciphertext = '';
+
+    axios.get("/documents.json")
+        .then(function (response) {
+            ciphertext = response.data;
+            console.log(ciphertext);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+
+    docform.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        // Decrypt
+        let decryptPw = docpw.value;
+        let decryptSalt = SHA256(decryptPw).toString();
+        let decryptedBytes  = AES.decrypt(ciphertext, decryptSalt).toString(ENCUTF8);
+
+        let success = true;
+        try {
+            decryptedBytes = JSON.parse(decryptedBytes);
+        }
+        catch (err) {
+            console.log('wrong password');
+            success = false;
+        }
+
+        if (success) {
+            docwrap.innerHTML = ''; // reset content
+            // loops topics and add headings
+            for (let i = 0; i < decryptedBytes.length; i++) {
+                let topic = decryptedBytes[i];
+                let headingNode = document.createElement('h3');
+                headingNode.innerHTML = topic.title;
+                docwrap.appendChild(headingNode);
+                // loop documents within topic and add download link
+                for (let j = 0; j < topic.documents.length; j++) {
+                    let doc = topic.documents[j];
+                    let spanNode = document.createElement('span');
+                    let anchorNode = document.createElement('a');
+                    anchorNode.href = doc.url;
+                    anchorNode.innerHTML = doc.title;
+                    spanNode.appendChild(anchorNode);
+                    docwrap.appendChild(spanNode);
+                }
+            }
+        }
+    });
+}
+
+
 
 /**
  * initialize masonry layout for cards
